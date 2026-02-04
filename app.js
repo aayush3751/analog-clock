8595900651a8219184ed27fc2a300c1d53dc20d5
Aayush kumar <aayush3751@gmail.com>
2026-02-05 01:06:29 +0530

    add input box to see different city time

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-setInterval(() => {
-    const date = new Date();
+const geourl = "https://geocoding-api.open-meteo.com/v1/search?";
+const city=document.querySelector("#name");
+const button=document.querySelector("#submit");
 
+async function  getZone(){
+    const response = await fetch(
+    geourl + "name=" + city.value + "&count=1"
+  );
+    let data=await response.json();
+   
+    console.log(data);
+    const zone=data.results[0].timezone;
+    startClock(zone);
+ }
+ function startClock(timeZone) {
+setInterval(() => {
+    const date = new Date(
+    new Date().toLocaleString("en-US", {timeZone})
+  );
     const htime = date.getHours();
     const mtime = date.getMinutes();
     const stime = date.getSeconds();
@@ -8,10 +24,10 @@ setInterval(() => {
     const hrotation = 30 * htime + mtime / 2;
     const mrotation = 6 * mtime;
     const srotation = 6 * stime;
-    console.log(stime);
-    console.log(htime);
-    console.log(mtime);
+    
     hour.style.transform = `rotate(${hrotation}deg)`;
     minute.style.transform = `rotate(${mrotation}deg)`;
     second.style.transform = `rotate(${srotation}deg)`;
 }, 1000);
+ }
+button.addEventListener("click",getZone);
